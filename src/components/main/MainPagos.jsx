dda9e4680b8921bac1c54f8e86d76ffa51ef8758
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-05-15 21:22:51 -0500

    Agregando iconos de boostrap para pintar las demas cards

--- src/components/main/MainPagos.jsx
+++ src/components/main/MainPagos.jsx
@@ -1,37 +1,42 @@
 import React from 'react'
 import { pagosTarjetas, pagosEfectivo } from '../../data/data';
 import { StylesMainPagos, InfoPagos, CompaniesTarjetas } from './StylesMainPagos';
+import { HeartFill } from 'react-bootstrap-icons';
 const MainPagos = () => {
     return (
         <StylesMainPagos>
-            <h1>Pago seguro</h1>
-            <InfoPagos>
-                <CompaniesTarjetas>
-                    <p>Tarjeta de crédito, tarjeta de débito y pago electrónico</p>
-                    <div>
-                        {
-                            pagosTarjetas.map((tarjeta) => (
-                            <figure>
-                                <img src={tarjeta.image} alt={tarjeta.title} />
-                            </figure>))
-                        }
-                    </div>
-                </CompaniesTarjetas>
-                <CompaniesTarjetas>
-                    <p>Efectivo en cualquiera de las sucursales principales</p>
-                    <div>
-                        {
-                            pagosEfectivo.map((tienda) => (
-                                <figure>
-                                    <img src={tienda.image} alt={tienda.name} />
-                                </figure>
-                            ))
-                        }
-                    </div>
-                </CompaniesTarjetas>
-            </InfoPagos>
             <div>
+                <h1>Pago seguro</h1>
+                <InfoPagos>
+                    <CompaniesTarjetas>
+                        <p>Tarjeta de crédito, tarjeta de débito y pago electrónico</p>
+                        <div>
+                            {
+                                pagosTarjetas.map((tarjeta) => (
+                                    <figure>
+                                        <img src={tarjeta.image} alt={tarjeta.title} />
+                                    </figure>))
+                            }
+                        </div>
+                    </CompaniesTarjetas>
+                    <CompaniesTarjetas>
+                        <p>Efectivo en cualquiera de las sucursales principales</p>
+                        <div>
+                            {
+                                pagosEfectivo.map((tienda) => (
+                                    <figure>
+                                        <img src={tienda.image} alt={tienda.name} />
+                                    </figure>
+                                ))
+                            }
+                        </div>
+                    </CompaniesTarjetas>
+                </InfoPagos>
+            </div>
 
+            <div>
+                <h1>Servicios disponibles</h1>
+                <HeartFill />
             </div>
         </StylesMainPagos>
     )
