79dcf2d751cbd171cd589fddeffbd7509fc3ea6f
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-05-18 22:30:07 -0500

    Ajustando styled components

--- src/components/main/StylesMainPagos.jsx
+++ src/components/main/StylesMainPagos.jsx
@@ -1,4 +1,4 @@
-import { styled } from "styled-components"
+import styled from "styled-components";
 
 export const StylesMainPagos = styled.main`
 @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
