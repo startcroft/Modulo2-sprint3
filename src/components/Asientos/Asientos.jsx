e417a9a77a3cedbd8b6eb5c63be7956e40f926fd
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-05-23 21:03:00 -0500

    Pintando primeros botones

--- src/components/Asientos/Asientos.jsx
+++ src/components/Asientos/Asientos.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 const Asientos = () => {
     const botones = [];
 
-  for (let i = 1; i <= 20; i++) {
-    botones.push(<button key={i}>Botón {i}</button>);
+  for (let i = 1; i <= 15; i++) {
+    botones.push(<button key={i} style={{padding:'20px', }}></button>);
   }
 
   return (
-    <div>{botones}</div>
+    <div>
+        <h3>Seleccione sus asientos</h3>
+        <div>
+
+        </div>
+        <div style={{display:'flex', flexDirection:'row', gap: '10px', width:'5%'}}>
+        {botones}
+        </div>
+        
+    </div>
   )
 }
 
